93851fc7da0a1075c3664bd9d9cf921467ae6b50
Priyanshu Vishwakarma <princevishwakarma126@gmail.com>
2026-06-12 02:19:11 +0530

    Add Student model class with fields, constructor, getters/setters

---
+++ src/Student.java
@@ -0,0 +1,67 @@
+public class Student {
+
+    private int id;
+    private String name;
+    private int age;
+    private String course;
+    private String email;
+
+    public Student(int id, int age, String name, String course, String email) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+        this.course = course;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                ", course='" + course + '\'' +
+                '}';
+    }
+}
